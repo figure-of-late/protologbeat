617ca8a87ac05e912dff6fa2c6f25bcd59250989
SG <13872653+mmguero@users.noreply.github.com>
2020-10-13 16:58:54 -0600

    try to fix build in 7.9.2

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,14 @@
 package cmd
 
 import (
-  "github.com/elastic/beats/libbeat/cmd"
-  "github.com/elastic/beats/libbeat/cmd/instance"
+	"os"
+
+	"github.com/elastic/beats/libbeat/beat"
+	"github.com/elastic/beats/libbeat/cmd"
+	"github.com/elastic/beats/libbeat/cmd/instance"
 
 	"github.com/mmguero-dev/protologbeat/beater"
 )
 
 // RootCmd to handle beats cli
-var RootCmd = cmd.GenRootCmdWithSettings(beater.DefaultCreator(), instance.Settings{Name: "protologbeat"})
+var RootCmd = cmd.GenRootCmdWithSettings(beater.New, instance.Settings{Name: "protologbeat"})
